5503c1b23413b02a1206ed600d6feaac935f862b
Aditya raj <aytida.dev@gmail.com>
2024-12-31 02:52:59 +0530

    sd

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const targetUrl = `http://15.207.165.204${req.url}`; // Your WordPress site URL
+  const targetUrl = `http://15.207.165.204`; // Your WordPress site URL
 
   // Fetch data from your WordPress site (proxy request)
   const response = await fetch(targetUrl);
